85d57c97986199cd45e2ce6544d0a69092a98856
quepunto-hub <quepunto@gmail.com>
2025-12-03 09:54:12 +0100

    page.tsx

--- src/app/legends/page.tsx
+++ src/app/legends/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+export const dynamic = 'force-dynamic'
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import { Card } from "@/components/ui/card"
